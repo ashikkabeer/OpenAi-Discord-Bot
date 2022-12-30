0261069fd929a2fa8558cd0e366bfde6a1d8acfb
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2022-12-30 15:55:40 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-// console.log("Hello AI bot")
+
 const { ask } = require("./ai.js");
 require('dotenv').config()
 const token = process.env.TOKEN
@@ -16,11 +16,10 @@ const client = new Client({
   
   client.on(Events.MessageCreate, async message => {
     if (message.content.substring(0, 1) === "!") {
-      const prompt = message.content.substring(1); //remove the exclamation mark from the message
-      const answer = await ask(prompt); //prompt GPT-3
+      const prompt = message.content.substring(1); 
+      const answer = await ask(prompt); 
       client.channels.fetch(message.channelId).then(channel => channel.send(answer));
     }
   });
   
-  // Log in to Discord with your client's token
   client.login(token);
